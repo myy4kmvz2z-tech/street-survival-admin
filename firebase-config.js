029b84e7c0d1bda95862782eb5c5e5b52e4036e2
myy4kmvz2z-tech <myy4kmvz2z@privaterelay.appleid.com>
2026-06-10 22:39:33 +0900

    Add Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+window.STREET_SURVIVAL_FIREBASE_ENABLED = true;
+
+window.STREET_SURVIVAL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDaUF6IiyaD3-oJ8pbR3KZzS6-njin-VLU",
+  authDomain: "street-survival-90e16.firebaseapp.com",
+  databaseURL: "https://street-survival-90e16-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "street-survival-90e16",
+  storageBucket: "street-survival-90e16.firebasestorage.app",
+  messagingSenderId: "67221657954",
+  appId: "1:67221657954:web:ec11417cddf2c38d6ff43b",
+  measurementId: "G-HTWXX6J5YN"
+};
